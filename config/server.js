5a98f24ce90affa09c5cd4f5a1f2a8b6220dd0eb
geodevcode <geodevinstitute@gmail.com>
2023-02-15 10:04:07 +0100

    env folder added

--- config/server.js
+++ config/server.js
@@ -1,6 +1,5 @@
 module.exports = ({ env }) => ({
-  // host: env('HOST', '0.0.0.0'),
-  host: env('https://goge-africa-backend-production.up.railway.app', '0.0.0.0'),
+  host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
   app: {
     keys: env.array('APP_KEYS',["testKey1", "testKey2"]),
